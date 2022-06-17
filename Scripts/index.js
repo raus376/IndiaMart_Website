08837da60798e26938ba96a18269379b2c17e9ba
Yashhh08 <yashyerunkar8@gmail.com>
2022-06-18 00:15:01 +0530

    added links to homepage

---
+++ Scripts/index.js
@@ -0,0 +1,82 @@
+document
+  .querySelector(".Y_link_foundation")
+  .addEventListener("click", function () {
+    window.location.href = "./IndiaMart_Website/productinfo.html";
+  });
+
+document.querySelector(".Y_link_scrub").addEventListener("click", function () {
+  window.location.href = "./IndiaMart_Website/scrubs.html";
+});
+
+document
+  .querySelector(".Y_link_lipstick")
+  .addEventListener("click", function () {
+    window.location.href = "./IndiaMart_Website/lipstick.html";
+  });
+
+document
+  .querySelector(".Y_link_bleachcream")
+  .addEventListener("click", function () {
+    window.location.href = "./IndiaMart_Website/bleachcream.html";
+  });
+
+// PRODUCT & SERVICES
+
+document
+  .querySelector("#Y_link_foundation")
+  .addEventListener("click", function () {
+    window.location.href = "./IndiaMart_Website/productinfo.html";
+  });
+
+document
+  .querySelector("#Y_link_lipstick")
+  .addEventListener("click", function () {
+    window.location.href = "./IndiaMart_Website/lipstick.html";
+  });
+
+document
+  .querySelector("#Y_link_lipproducts")
+  .addEventListener("click", function () {
+    window.location.href = "./IndiaMart_Website/lipsProducts.html";
+  });
+
+document
+  .querySelector("#Y_link_bleachcream")
+  .addEventListener("click", function () {
+    window.location.href = "./IndiaMart_Website/bleachcream.html";
+  });
+
+document.querySelector("#Y_link_scrubs").addEventListener("click", function () {
+  window.location.href = "./IndiaMart_Website/scrubs.html";
+});
+
+document
+  .querySelector("#Y_link_eyeproducts")
+  .addEventListener("click", function () {
+    window.location.href = "./IndiaMart_Website/eyesProducts.html";
+  });
+
+document
+  .querySelector("#Y_link_lipcare")
+  .addEventListener("click", function () {
+    window.location.href = "./IndiaMart_Website/lipcare.html";
+  });
+
+document
+  .querySelector("#Y_link_traditional")
+  .addEventListener("click", function () {
+    window.location.href = "./IndiaMart_Website/eyesProducts.html";
+  });
+
+document
+  .querySelector("#Y_product_services_button")
+  .addEventListener("click", function () {
+    window.location.href = "./IndiaMart_Website/productinfo.html";
+  });
+
+//   Ready to Ship Products in Similar Categories
+
+document.querySelector("form").addEventListener("submit", function () {
+  event.preventDefault();
+  alert("Thanks..!! We will get back to you shortly.");
+});
